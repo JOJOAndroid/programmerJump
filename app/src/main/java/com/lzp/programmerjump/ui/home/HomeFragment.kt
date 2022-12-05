ec7d1a2ae1f50beadd9b2143956794f91a2fe627
luzp <zhanpeng.lu@beantechs.com>
2022-12-05 09:45:45 +0800

    init

---
+++ app/src/main/java/com/lzp/programmerjump/ui/home/HomeFragment.kt
@@ -0,0 +1,45 @@
+package com.lzp.programmerjump.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.lzp.programmerjump.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    var homeViewModel: HomeViewModel ?= null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        initObserve()
+        return root
+    }
+
+    fun initObserve() {
+        homeViewModel?.text?.observe(viewLifecycleOwner) {
+            binding.textHome.text = it
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
